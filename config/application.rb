fb87bbc994162195239543618461144487a50848
S.m zaki ahmed <zakiahmed2010@live.com>
2017-04-24 13:07:03 +0500

    sss

--- config/application.rb
+++ config/application.rb
@@ -8,8 +8,10 @@ Bundler.require(*Rails.groups)
 
 module Blogs
   class Application < Rails::Application
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.assets.initialize_on_precompile=false
   end
 end
